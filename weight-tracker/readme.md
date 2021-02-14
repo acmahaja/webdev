04806040607d8ac5dfc26b1d8cec5285cf6deedf
acmahaja <acmahaja@gmail.com>
2021-02-14 22:03:47 +0200

    Restarted Project

--- weight-tracker/readme.md
+++ weight-tracker/readme.md
@@ -1,4 +1,7 @@
+#V1
 It accepts a set of manual entries of weight measurements taken at different dates
 It can plot a graph
-It can allow to track multiple entities, for example more than one person weight
 Store them somewhere
+
+#V2
+It can allow to track multiple entities, for example more than one person weight
---
+++ weight-tracker/seed.js
@@ -0,0 +1,92 @@
+var d = new Date();
+d.setDate(d.getDate() - 0);
+const d0 = d.toLocaleString();
+d.setDate(d.getDate() - 1);
+const d1 = d.toLocaleString();
+d.setDate(d.getDate() - 1);
+const d2 = d.toLocaleString();
+d.setDate(d.getDate() - 1);
+const d3 = d.toLocaleString();
+d.setDate(d.getDate() - 1);
+const d4 = d.toLocaleString();
+d.setDate(d.getDate() - 1);
+const d5 = d.toLocaleString();
+
+
+module.exports = {
+    "03055c10-c50a-42e8-bf4e-53cdc091b7e6": {
+        "email": "jake@mail.com",
+        "name": "Jake Josslyn",
+        "password": "password",
+        "unit": 1,
+        "color": "#ef00ff",
+        "measurements": [
+            { "date": d0, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d1, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d2, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d3, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d4, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d5, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+        ]
+    },
+    "9e342952-5b60-4d4e-83da-5d75b89582f4": {
+        "email": "donna@mail.com",
+        "name": "Donna Zandra",
+        "password": "password",
+        "unit": 1,
+        "color": "#ef00ff",
+        "measurements": [
+            { "date": d0, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d1, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d2, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d3, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d4, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d5, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+        ]
+    },
+    "810850e0-69f1-4fc3-a722-af1c10745c7c": {
+        "email": "mitch@mail.com",
+        "name": "Mitch Jordan",
+        "password": "password",
+        "unit": 1,
+        "color": "#ef00ff",
+        "measurements": [
+            { "date": d0, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d1, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d2, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d3, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d4, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d5, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+        ]
+    },
+    "28a4ec9b-4d6f-4afd-8fb8-10ab2cb2b66a": {
+        "email": "susanna@mail.com",
+        "name": "Susanna Kiefer",
+        "password": "password",
+        "unit": 1,
+        "color": "#ef00ff",
+        "measurements": [
+            { "date": d0, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d1, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d2, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d3, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d4, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d5, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+        ]
+    },
+    "2096a2a6-6827-4d28-b374-61169b99934b": {
+        "email": "averie@mail.com",
+        "name": "Averie Garth",
+        "password": "password",
+        "unit": 1,
+        "color": "#ef00ff",
+        "measurements": [
+            { "date": d0, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d1, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d2, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d3, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d4, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+            { "date": d5, "value": Math.floor(Math.random() * 30 + 60), unit: 1 },
+        ]
+    },
+}
